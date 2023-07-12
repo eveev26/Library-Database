ad817f405760004400c955b8d716f39496a09687
eveev26 <89487054+eveev26@users.noreply.github.com>
2023-07-11 20:01:01 -0400

    Added book details page

--- librarydb/database/urls.py
+++ librarydb/database/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<int:book_id>/", views.detail, name="detail")
 ]
--- librarydb/database/views.py
+++ librarydb/database/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, Http404
 from .models import Book, LibraryBranch
 from django.template import loader
 
@@ -10,6 +10,14 @@ def index(request):
         "book_list": book_list,
     }
     return HttpResponse(template.render(context, request))
-    # output = ", ".join([b.title for b in book_list])
-    # return HttpResponse(output)
     
+def detail(request, book_id):
+    book = get_object_or_404(Book, pk=book_id)
+    return render(request, "database/detail.html", {"book": book})
+
+# def detail(request, book_id):
+#     try:
+#         book = Book.objects.get(pk=book_id)
+#     except Book.DoesNotExist:
+#         raise Http404("Book does not exist")
+#     return render(request, "database/detail.html", {"book": book})
