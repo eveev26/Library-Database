89e26a5a6734b1a56e6f0d331ead78f165a36ea0
eveev26 <89487054+eveev26@users.noreply.github.com>
2023-07-13 01:35:06 -0400

    Fixed models (again)

--- librarydb/database/views.py
+++ librarydb/database/views.py
@@ -5,15 +5,18 @@ from django.template import loader
 
 def index(request):
     book_list = Book.objects.order_by("-title")
+    library_list = LibraryBranch.objects.order_by("-branch_name")
     template = loader.get_template("database/index.html")
     context = {
         "book_list": book_list,
+        "library_list": library_list,
     }
     return HttpResponse(template.render(context, request))
     
 def detail(request, book_id):
     book = get_object_or_404(Book, pk=book_id)
-    return render(request, "database/detail.html", {"book": book})
+    library_list = LibraryBranch.objects.all()
+    return render(request, "database/detail.html", {"book": book, "library_list": library_list})
 
 # def detail(request, book_id):
 #     try:
