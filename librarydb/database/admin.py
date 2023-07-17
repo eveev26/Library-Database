87779168137228037cc3d893623969d815e5a577
eveev26 <89487054+eveev26@users.noreply.github.com>
2023-07-16 23:27:59 -0400

    ok i have fixed the models now...

--- librarydb/database/admin.py
+++ librarydb/database/admin.py
@@ -1,19 +1,13 @@
 from django.contrib import admin
 
-from .models import LibraryBranch, User, Activity, Book
+from .models import User, Activity, Book, BooksAvailable, LibraryBranch
 
 class BookInline(admin.StackedInline):
     model = Book
     extra = 3
 
-class LibraryBranchAdmin(admin.ModelAdmin):
-    fieldsets = [
-        (None, {"fields": ["branch_name", "address"]}),
-        # ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
-    ]
-    inlines = [BookInline]
-
-admin.site.register(LibraryBranch)
 admin.site.register(User)
 admin.site.register(Activity)
 admin.site.register(Book)
+admin.site.register(BooksAvailable)
+admin.site.register(LibraryBranch)
